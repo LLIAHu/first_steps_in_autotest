b803dd57aa966510dfaf74b1cd773468ddc52419
LLIAHu <119920684+LLIAHu@users.noreply.github.com>
2022-12-09 15:37:36 +0300

    Update lesson1.6_send_keys.py

--- lesson1.6_send_keys.py
+++ lesson1.6_send_keys.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time 
 
-# link = "http://suninjuly.github.io/simple_form_find_task.html"
+link = "http://suninjuly.github.io/simple_form_find_task.html"
 
 try:
     browser = webdriver.Chrome()
